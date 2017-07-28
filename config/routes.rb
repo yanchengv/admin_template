acd0d86e76703f50123b87b15df374d632754d86
yancheng <zqalyc@163.com>
2017-07-28 16:44:51 +0800

    login

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
 
   resources :sessions
   resources :homes
-  resources :admins
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
